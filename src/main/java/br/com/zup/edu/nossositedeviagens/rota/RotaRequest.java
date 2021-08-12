7248920a4303127342825139132f438735f1ab80
jeffersonzup <jefferson.wiltenburg@zup.com.br>
2021-08-12 17:24:29 -0300

    piloto: Jefferson

--- src/main/java/br/com/zup/edu/nossositedeviagens/rota/RotaRequest.java
+++ src/main/java/br/com/zup/edu/nossositedeviagens/rota/RotaRequest.java
@@ -4,6 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.Duration;
+import java.util.Objects;
 
 public class RotaRequest {
 
@@ -16,14 +17,28 @@ public class RotaRequest {
     private String destino;
     @Positive
     @NotNull
-    private Duration duracao;
+    private Integer duracao;
 
 
-    public RotaRequest(String nome, String origem, String destino, Duration duracao) {
-        this.nome = nome;
+    public RotaRequest(String nome, String origem, String destino, Integer duracao) {
+        this.nome = validarNomeRota(nome);
         this.origem = origem;
         this.destino = destino;
         this.duracao = duracao;
+    }
+
+    private String validarNomeRota(String nome){
+        //se der merda foi aqui
+        if(Objects.isNull(this.nome)){
+            return this.origem + " - " + this.destino;
+        }
+        return nome;
+    }
+
+    private String validaOrigemDestino (String origem, String destino){
+        if(origem.equals(destino)){
+            throw new RegraDeNegocioException("Não pode ");
+        }
     }
 
 }
